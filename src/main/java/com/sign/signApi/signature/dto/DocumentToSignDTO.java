16197833403b4ad4e20cab731763de4c8dfac3c2
santiago.gutierrez <santiago.gutierrez@udc.es>
2025-12-06 14:34:04 +0100

    [Fix]: set a max length to the string with the document in base64

--- src/main/java/com/sign/signApi/signature/dto/DocumentToSignDTO.java
+++ src/main/java/com/sign/signApi/signature/dto/DocumentToSignDTO.java
@@ -1,10 +1,12 @@
 package com.sign.signApi.signature.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class DocumentToSignDTO {
     @NotBlank(message = "Document cannot be empty")
+    @Size(max = 35000000, message = "Document too large")
     private String documentBase64;
 }
